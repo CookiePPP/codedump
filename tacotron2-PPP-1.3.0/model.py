f0f50b4cccd30f59f3e45d6f9bcc76de1704776a
Cookie <42448678+CookiePPP@users.noreply.github.com>
2021-02-15 16:29:08 +0000

    updated Zoneout inference mode to be paper accurate.

--- tacotron2-PPP-1.3.0/model.py
+++ tacotron2-PPP-1.3.0/model.py
@@ -43,7 +43,8 @@ class LSTMCellWithZoneout(nn.LSTMCell):
             c = torch.where(c_mask, old_c, new_c)
             return h, c
         else:
-            return new_h, new_c
+            not_zoneout_prob = 1.0-self._zoneout_prob
+            return old_h*self._zoneout_prob+new_h*not_zoneout_prob, old_c*self._zoneout_prob+new_c*not_zoneout_prob
 
 
 class LocationLayer(nn.Module):
